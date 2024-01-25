739ced597699fb467630872b10e6a43b09893c2f
stiven2610 <stivenrozo1@gmail.com>
2024-01-25 16:57:31 -0500

    actualizacion

---
+++ vista/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./componentes/Header/Header"
+import Footer from "./componentes/Footer/Footer"
+import Padre   from "./componentes/padre/padre"
+function App() {
+    return (
+      <>
+        <Header/>
+        <Footer/>
+      </>
+    )
+  }
+  
+  export default App;
+  
