813c3af308f4e9f9720cbe48c4b2c173b83d3543
stiven <stivenrozo1@gmail.com>
2024-02-05 10:46:11 -0500

    stilos

--- vista/src/componentes/adjudicados/adjudicados.jsx
+++ vista/src/componentes/adjudicados/adjudicados.jsx
@@ -1,6 +1,3 @@
-
-
-
 import { useEffect, useState } from "react";
 import "./styles.css";
 import Navadministrador from "../Navadministrador/navAdministrador";
@@ -42,108 +39,122 @@ const TablaAdjudicados = () => {
   };
 
   const filteredDatos = datos.filter((item) => {
-    const estadoMatch = filtroEstado === "" || String(item.id_estado_aprendiz) === filtroEstado;
+    const estadoMatch =
+      filtroEstado === "" || String(item.id_estado_aprendiz) === filtroEstado;
     const busquedaMatch =
-      item.nombre_completo_aprendiz.toLowerCase().includes(filtroBusqueda.toLowerCase()) ||
+      item.nombre_completo_aprendiz
+        .toLowerCase()
+        .includes(filtroBusqueda.toLowerCase()) ||
       String(item.numero_documento_aprendiz).includes(String(filtroBusqueda));
     return estadoMatch && busquedaMatch;
   });
 
   return (
     <>
-    <Navadministrador/>
-    <div className="container mt-4">
-      <h4 className="mb-4">Gestión de Aprendices adjudicados</h4>
-      <div className="row mb-3">
-        <div className="col-md-6">
-          <label className="text-dark">Filtrar por Estado de Aprendiz:</label>
-          <select className="form-control" value={filtroEstado} onChange={handleEstadoChange}>
-            <option value="">Todos</option>
-            <option value="1">Aprendices a punto de cumplir etapa lectiva</option>
-            <option value="2">Aprendices en mes de gracia</option>
-            <option value="3">Aprendices en proyecto productivo</option>
-            <option value="4">Aprendices Aplazados</option>
-            <option value="5">Aprendices en etapa lectiva</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label className="text-dark">Buscar por Nombre o Número de Documento:</label>
-          <div className="input-group">
+      <Navadministrador />
+      <div className="container ">
+        <h4 className="mb-4">Gestión de Aprendices adjudicados</h4>
+        <div className="container-filtros">
+          <div className="container-filtro col-md-4 mb-3">
+            <label htmlFor="estado" className="text-dark">
+              Filtrar por Estado de Aprendiz:
+            </label>
+            <select
+              id="estado"
+              className="form-control"
+              value={filtroEstado}
+              onChange={handleEstadoChange}
+            >
+              <option value="">Todos</option>
+              <option value="1">
+                Aprendices a punto de cumplir etapa lectiva
+              </option>
+              <option value="2">Aprendices en mes de gracia</option>
+              <option value="3">Aprendices en proyecto productivo</option>
+              <option value="4">Aprendices Aplazados</option>
+              <option value="5">Aprendices en etapa lectiva</option>
+            </select>
+          </div>
+
+          <div className="container-buscador col-md-4 mb-3">
+            <label htmlFor="busqueda" className="text-dark">
+              Buscar por Nombre o Número de Documento:
+            </label>
             <input
               type="text"
+              id="busqueda"
               className="form-control"
               value={filtroBusqueda}
               onChange={handleBusquedaChange}
             />
-            <div className="input-group-append">
-              <button
-                className="btn btn-success "
-                style={{ backgroundColor: "#45ca03", borderColor: "#45ca03" }}
-                onClick={agregarNuevoAprendiz}
-              >
-                Agregar Nuevo Aprendiz
-              </button>
-            </div>
+          </div>
+
+          <div className="container-boton col-md-4 mb-3">
+            <button
+              className="boton "
+              onClick={agregarNuevoAprendiz}
+            >
+            Adjudicar Aprendiz
+            </button>
           </div>
         </div>
-      </div>
-      <div className="table-responsive">
-        <table className="table table-bordered table-striped">
-          <thead style={{ backgroundColor: "#45ca03", color: "#fff" }}>
-            <tr>
-              <th>GESTIONAR</th>
-              <th>Número Consecutivo</th>
-              <th>Nombre Completo del Aprendiz</th>
-              <th>Tipo de Documento</th>
-              <th>Número de Documento</th>
-              <th>Estado de Aprendiz</th>
-              <th>Obligación Mensual</th>
-              <th>Código de Ficha</th>
-              <th>Codigo de beneficio</th>
-              <th>Fecha de Adjudicación</th>
-              <th>Modalidad</th>
-              <th>Teléfono Fijo</th>
-              <th>Teléfono Móvil</th>
-              <th>Dirección de Residencia del Aprendiz</th>
-              <th>Correo Electrónico del Aprendiz</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cargando ? (
-              <tr>
-                <td colSpan="15">Cargando datos...</td>
-              </tr>
-            ) : (
-              filteredDatos.map((item) => (
-                <tr key={item.numero_documento_aprendiz}>
-                  <td>
-                    <div className="iconos_gestion d-flex flex-column align-items-center ">
-                      <button className="btn btn-primary">Editar</button>
-                    </div>
-                  </td>
-                  <td>{item.numero_consecutivo}</td>
-                  <td>{item.nombre_completo_aprendiz}</td>
-                  <td>{item.id_tipo_documento}</td>
-                  <td>{item.numero_documento_aprendiz}</td>
-                  <td>{item.id_estado_aprendiz}</td>
-                  <td>{item.id_obligacion_mensual}</td>
-                  <td>{item.codigo_ficha}</td>
-                  <td>{item.codigo_beneficio}</td>
-                  <td>{item.fecha_adjudicacion}</td>
-                  <td>{item.id_modalidad}</td>
-                  <td>{item.numero_telefono_fijo}</td>
-                  <td>{item.numero_telefono_movil}</td>
-                  <td>{item.direccion_residencia_aprendiz}</td>
-                  <td>{item.email_aprendiz}</td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
-    </>
 
+        <div className="table-responsive">
+          <table className="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th>GESTIONAR</th>
+                <th>Número Consecutivo</th>
+                <th>Nombre Completo del Aprendiz</th>
+                <th>Tipo de Documento</th>
+                <th>Número de Documento</th>
+                <th>Estado de Aprendiz</th>
+                <th>Obligación Mensual</th>
+                <th>Código de Ficha</th>
+                <th>Codigo de beneficio</th>
+                <th>Fecha de Adjudicación</th>
+                <th>Modalidad</th>
+                <th>Teléfono Fijo</th>
+                <th>Teléfono Móvil</th>
+                <th>Dirección de Residencia del Aprendiz</th>
+                <th>Correo Electrónico del Aprendiz</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cargando ? (
+                <tr>
+                  <td colSpan="15">Cargando datos...</td>
+                </tr>
+              ) : (
+                filteredDatos.map((item) => (
+                  <tr key={item.numero_documento_aprendiz}>
+                    <td>
+                      <div className="iconos_gestion d-flex flex-column align-items-center ">
+                        <button className="btn btn-primary">Editar</button>
+                      </div>
+                    </td>
+                    <td>{item.numero_consecutivo}</td>
+                    <td>{item.nombre_completo_aprendiz}</td>
+                    <td>{item.id_tipo_documento}</td>
+                    <td>{item.numero_documento_aprendiz}</td>
+                    <td>{item.id_estado_aprendiz}</td>
+                    <td>{item.id_obligacion_mensual}</td>
+                    <td>{item.codigo_ficha}</td>
+                    <td>{item.codigo_beneficio}</td>
+                    <td>{item.fecha_adjudicacion}</td>
+                    <td>{item.id_modalidad}</td>
+                    <td>{item.numero_telefono_fijo}</td>
+                    <td>{item.numero_telefono_movil}</td>
+                    <td>{item.direccion_residencia_aprendiz}</td>
+                    <td>{item.email_aprendiz}</td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
