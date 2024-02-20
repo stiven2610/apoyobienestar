e6d305300a25be9e05bd0a477fd09fd204706c16
unknown <Aprendiz@BUCDFPCSEFSD163.FormacionSena.RED>
2024-02-20 17:01:55 -0500

    store procedure por hacer update

--- modelo/postgresql/fun_asis_taller.sql
+++ modelo/postgresql/fun_asis_taller.sql
@@ -1,9 +1,20 @@
+CREATE OR REPLACE PROCEDURE fun_reg_asis(numero_documento_aprendiz_param INT, codigo_taller INT) 
+LANGUAGE PLPGSQL
+AS $$
+DECLARE 
+    documento INT;
+BEGIN 
+    -- Seleccionar el campo 'numero_documento_aprendiz' y asignarlo a la variable 'documento'
+    SELECT aprendiz.numero_documento_aprendiz INTO documento
+    FROM aprendiz
+    WHERE aprendiz.numero_documento_aprendiz = numero_documento_aprendiz_param;
 
-CREATE SEQUENCE codigo_seq
-START WITH 10001
-INCREMENT BY 1
-NO MAXVALUE
-NO MINVALUE
-CACHE 1;
+    -- Imprimir el valor de 'documento' utilizando RAISE NOTICE
+    RAISE NOTICE 'El valor de documento es: %', documento;
 
+END
+$$;
 
+select * from aprendiz ;
+call fun_reg_asis(109477878,11036);
+select * from taller_mensual;
