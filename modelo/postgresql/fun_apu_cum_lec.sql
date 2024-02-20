ac0eeec89ae5cb087b2378462d9ac5edcf874bf3
stiven <stivenrozo1@gmail.com>
2024-02-20 08:45:05 -0500

    cambio en store procedure estados

--- modelo/postgresql/fun_apu_cum_lec.sql
+++ modelo/postgresql/fun_apu_cum_lec.sql
@@ -1,4 +1,3 @@
-call fun_apu_cum_lec();
 CREATE OR REPLACE PROCEDURE fun_apu_cum_lec()
 LANGUAGE plpgsql
 AS $$
@@ -19,20 +18,22 @@ BEGIN
         -- Calcular los días transcurridos desde la fecha de inicio de la etapa productiva
         dias_transcurridos := fecha_actual - fecha_inicio;
 
-        IF dias_transcurridos >= 15 THEN
-		 RAISE NOTICE 'EL APRENDIZ ESTA POR CUMPLIR ETAPA LECTIVA';
-		 
-            UPDATE aprendiz SET id_estado_aprendiz = 1 WHERE numero_documento_aprendiz = numero_documento; 
-        ELSE
-            RAISE NOTICE 'EL APRENDIZ AÚN SE ENCUENTRA EN ETAPA LECTIVA';
-        END IF;
+		IF dias_transcurridos >= 0 AND dias_transcurridos <= 15 THEN
+   			 RAISE NOTICE 'EL APRENDIZ ESTA POR CUMPLIR ETAPA LECTIVA';
+				ELSIF dias_transcurridos >= -15 AND dias_transcurridos < 0 THEN 
+   					 RAISE NOTICE 'EL APRENDIZ ENTRO A MES DE GRACIA';
+							ELSIF dias_transcurridos > 30 THEN
+   								 RAISE NOTICE 'EL APRENDIZ FUE APLAZADO PORQUE NO DEFINIÓ ETAPA PRÁCTICA';
+									ELSE 
+   										 RAISE NOTICE 'EL APRENDIZ AUN SE ENCUENTRA EN ETAPA LECTIVA';
+		END IF;
 
     END LOOP;
 END;
 $$;
 
 
-
+call fun_apu_cum_lec();
 select * from aprendiz;
 select * from estado_aprendiz;
-UPDATE ficha SET fecha_inicio_etapa_productiva   = '2024-02-18' where codigo_ficha = 2619702;
+UPDATE ficha SET fecha_inicio_etapa_productiva   = '2024-03-01' where codigo_ficha = 2619702;
