a9fcbd350c7a2383b46cdb1aa300cbc6247e12a5
unknown <Aprendiz@BUCDFPCSEFSD163.FormacionSena.RED>
2024-02-20 17:04:36 -0500

    store procedure por hacer update

--- modelo/postgresql/fun_actu_estado.sql
+++ modelo/postgresql/fun_actu_estado.sql
@@ -1,37 +1,48 @@
-call fun_apu_cum_lec();
-CREATE OR REPLACE PROCEDURE fun_apu_cum_lec()
+CREATE OR REPLACE PROCEDURE fun_act_est()
 LANGUAGE plpgsql
 AS $$
 DECLARE
     fecha_actual DATE;
     numero_documento INT;
     codigo INT;
-    fecha_inicio DATE;
-    dias_transcurridos INT;
+    fecha_fin DATE;
+    dias INT;
 BEGIN
     fecha_actual := CURRENT_DATE;
     
-    FOR numero_documento, codigo, fecha_inicio IN
-        SELECT a.numero_documento_aprendiz, a.codigo_ficha, b.fecha_inicio_etapa_productiva 
+    FOR numero_documento, codigo, fecha_fin IN
+        SELECT a.numero_documento_aprendiz, a.codigo_ficha, b.fecha_fin_lectiva
         FROM aprendiz AS a, ficha AS b 
         WHERE a.codigo_ficha = b.codigo_ficha
     LOOP
-        -- Calcular los días transcurridos desde la fecha de inicio de la etapa productiva
-        dias_transcurridos := fecha_actual - fecha_inicio;
+        dias := fecha_fin - fecha_actual;
 
-        IF dias_transcurridos <= 15 THEN
-            UPDATE aprendiz SET id_estado_aprendiz = 1 WHERE numero_documento_aprendiz = numero_documento; 
-        ELSE
-            RAISE NOTICE 'EL APRENDIZ AÚN SE ENCUENTRA EN ETAPA LECTIVA';
+        IF dias <= 15 AND dias > 0 THEN
+		update aprendiz set id_estado_aprendiz = "?" where numero_documento = numero_documento_aprendiz;
+            RAISE NOTICE 'dias :%',dias;
+            RAISE NOTICE 'codigo ficha :%',codigo;
+            RAISE NOTICE 'POR CUMPLIR ETAPA LECTIVA';
+        ELSIF fecha_fin = fecha_actual AND dias <= 30 THEN
+            RAISE NOTICE 'dias :%',dias;
+            RAISE NOTICE 'codigo ficha :%',codigo;
+            RAISE NOTICE 'mes de gracia';
+        ELSIF dias <=  0 THEN
+            RAISE NOTICE 'dias :%',dias;
+            RAISE NOTICE 'codigo ficha :%',codigo;
+            RAISE NOTICE 'aplazado';
+        ELSE 
+            RAISE NOTICE 'dias :%',dias;
+            RAISE NOTICE 'codigo ficha :%',codigo;
+            RAISE NOTICE 'AUN SE ENCUENTRA EN ETAPA LECTIVA';
         END IF;
-
-        RAISE NOTICE 'Número de documento: %, Código de ficha: %, Fecha de inicio: %', numero_documento, codigo, fecha_inicio;
     END LOOP;
 END;
 $$;
 
 
 
+select * from ficha ;
+call fun_act_est()
 select * from aprendiz;
 select * from estado_aprendiz;
-UPDATE ficha SET fecha_inicio_etapa_productiva   = '2024-02-18' where codigo_ficha = 2619702;
+UPDATE ficha SET fecha_fin_lectiva   = '2024-03-05' where codigo_ficha = 2670123;
