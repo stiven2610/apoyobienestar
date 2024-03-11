0af7c76ab170312d05def6419c3da59ab96091f2
stiven <stivenrozo1@gmail.com>
2024-03-11 10:31:05 -0500

    cambios en procedimientos

--- modelo/postgresql/fun_tri_nov.sql
+++ modelo/postgresql/fun_tri_nov.sql
@@ -1,36 +1,53 @@
 CREATE OR REPLACE FUNCTION fun_tri_nov() RETURNS TRIGGER AS 
 $$
+DECLARE
+    fecha_actual DATE; 
+    numero_documento INT;
+    estado_actual INT;
 BEGIN 
-    IF OLD.id_estado_aprendiz <> NEW.id_estado_aprendiz THEN
-        IF TG_OP = 'UPDATE' THEN 
-            IF NEW.id_estado_aprendiz = 1 THEN 
-                INSERT INTO novedad (id_novedad,id_tipo_novedad,numero_documento_aprendiz,usuario,fecha_novedad) VALUES ('default',1,NEW.numero_documento_aprendiz,'Sistema',NOW());
-                RAISE NOTICE 'La inserción fue realizada correctamente para estado 1.';
-            ELSIF NEW.id_estado_aprendiz = 2 THEN 
-                INSERT INTO novedad (id_novedad,id_tipo_novedad,numero_documento_aprendiz,usuario,fecha_novedad) VALUES ('default',2,NEW.numero_documento_aprendiz,'Sistema',NOW());
-                RAISE NOTICE 'La inserción fue realizada correctamente para estado 2.';
-            ELSIF NEW.id_estado_aprendiz = 4 THEN 
-                INSERT INTO novedad (id_novedad,id_tipo_novedad,numero_documento_aprendiz,usuario,fecha_novedad) VALUES ('default',4,NEW.numero_documento_aprendiz,'Sistema',NOW());
-                RAISE NOTICE 'La inserción fue realizada correctamente para estado 4.';
-            ELSIF NEW.id_estado_aprendiz = 5 THEN 
-                INSERT INTO novedad (id_novedad,id_tipo_novedad,numero_documento_aprendiz,usuario,fecha_novedad) VALUES ('default',5,NEW.numero_documento_aprendiz,'Sistema',NOW());
-                RAISE NOTICE 'La inserción fue realizada correctamente para estado 5.';
+    fecha_actual = CURRENT_DATE;
+    
+    IF TG_OP = 'UPDATE' THEN 
+        estado_actual = new.id_estado_aprendiz;
+        numero_documento = new.numero_documento_aprendiz;
+        
+        IF old.id_estado_aprendiz != estado_actual THEN -- Verificar si el estado ha cambiado
+            IF estado_actual = 1 THEN 
+                INSERT INTO novedad (id_tipo_novedad, numero_documento_aprendiz, usuario, fecha_novedad) 
+                VALUES (1, numero_documento, 'Sistema', fecha_actual);
+                RAISE NOTICE 'La inserción fue realizada correctamente para nuevo estado 1.';
+            ELSIF estado_actual = 2 THEN 
+                INSERT INTO novedad (id_tipo_novedad, numero_documento_aprendiz, usuario, fecha_novedad) 
+                VALUES (2, numero_documento, 'Sistema', fecha_actual);
+                RAISE NOTICE 'La inserción fue realizada correctamente para nuevo estado 2.';
+            ELSIF estado_actual = 4 THEN 
+                INSERT INTO novedad (id_tipo_novedad, numero_documento_aprendiz, usuario, fecha_novedad) 
+                VALUES (4, numero_documento, 'Sistema', fecha_actual);
+                RAISE NOTICE 'La inserción fue realizada correctamente para nuevo estado 4.';
+            ELSIF estado_actual = 3 THEN 
+                INSERT INTO novedad (id_tipo_novedad, numero_documento_aprendiz, usuario, fecha_novedad) 
+                VALUES (3, numero_documento, 'Sistema', fecha_actual);
+                RAISE NOTICE 'La inserción fue realizada correctamente para nuevo estado 3.';
             ELSE 
                 RAISE NOTICE 'No se realizó ninguna inserción de novedad.';
             END IF;
+        ELSE
+            RAISE NOTICE 'No se realizó ninguna inserción de novedad porque el estado no ha cambiado.';
         END IF;
     END IF;
-EXCEPTION
-    WHEN others THEN
-        RAISE NOTICE 'Error SQLSTATE en fun_tri: %', SQLSTATE; -- Uso de SQLSTATE para obtener el estado SQL actual
+    
+    RETURN new;
 END;
 $$
 LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE TRIGGER tri_act_tab_est 
+AFTER UPDATE OF id_estado_aprendiz ON aprendiz 
+FOR EACH ROW
+EXECUTE PROCEDURE fun_tri_nov(); 
+
+
 select * from novedad;
 select * from aprendiz;
 delete from novedad;
-CREATE TRIGGER tri_act_tab_est 
-AFTER UPDATE OF id_estado_aprendiz ON aprendiz 
-FOR EACH ROW
-EXECUTE FUNCTION fun_tri_nov();
